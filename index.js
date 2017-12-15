e60e12fe18ec1d06dc39ae207cdbe1aa2faf7188
josephkmh <jmarshmenthowell@gmail.com>
2017-12-15 12:04:34 +0100

    changed api port to 3001, added react client ignore

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.port || 3001;
 
 // allow CORS
 app.use(cors());
